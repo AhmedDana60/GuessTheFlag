f39c2f0ccd8ccea65782b058e639917cd04611af
AhmedDana60 <ahmeddanamo@gmail.com>
2024-04-20 10:18:05 +0300

    GuessTheFlagGame

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ahmed Dana Mohammed on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
